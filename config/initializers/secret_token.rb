d924d07464b9b1ad4c961d126cb340217154d7b6
Marcus Ortiz <mportiz08@gmail.com>
2013-02-01 11:54:21 -0800

    Created rails project using mysql and sequel.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Analytics::Application.config.secret_token = '3d52efb9ba23d7d335b57320d76e9faaa22e0d781fed824b280df621164c0f9794bf14d59e23ed1a0f44a1d3b52632558c2a5be0e6a0417e56af1c8248363431'
